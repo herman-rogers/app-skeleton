7432dca391b3d64a7d4d641aa5696bb2edd7dd54
Herman Rogers <herman.rogers@gmail.com>
2018-05-30 15:40:20 +0200

    Clean up components and styles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import HomePage from '../home/HomePage';
 import AppStyles from './app.style';
 
 const App = () => (
   <AppStyles>
     <div className="page-body">
       <Route exact path="/" component={() => <Redirect to="/home" />} />
-      <
+      <Route path="/home" component={HomePage} />
     </div>
     <div className="footer">
       Footer
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'antd/dist/antd.min.css';
-import './index.css';
-import App from './App';
+import { injectGlobal } from 'styled-components';
+import App from './app/App';
+
+/* eslint-disable */
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', sans-serif;
+  }
+`;
+/* eslint-enable */
 
 ReactDOM.render(
   <BrowserRouter>
